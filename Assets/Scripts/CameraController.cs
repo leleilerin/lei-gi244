94ab8b4f90889c2456bd1823564e23d147b28a07
leleilerin <sirinutthawee@gmail.com>
2024-01-24 00:18:03 +0700

    CameraController_1

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Camera cam;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Move")] 
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float zInput;
+
+    public static CameraController instance;
+
 }
